e1d97dbc3636c6a600798e2fa0a2eb4a81f0d8bc
kshitijshriyan14 <kshitijshriyan49@gmail.com>
2026-04-28 22:12:41 +1000

    Structure and layout

---
+++ a2/index.php
@@ -0,0 +1,12 @@
+<?php
+$title = "Home";
+include "includes/header.inc";
+include "includes/nav.inc";
+?>
+
+<main class="container mt-4">
+<h1>Welcome to PetConnect</h1>
+<p>Day 1 layout working</p>
+</main>
+
+<?php include "includes/footer.inc"; ?>
